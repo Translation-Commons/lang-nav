conradarcturus/lang-metadata

Hierarchy: Add viewing options and update some language families
I'm preparing a report to IANA on oddities in language families and while I was collecting data I saw a few opportunities to fix the data and add visual display toggles.

|View|Before|After|After(Toggled)
|--|--|--
|[French languages](https://translation-commons.github.io/lang-nav/?view=Hierarchy&languageSchema=ISO&searchBy=English+Name&searchString=French)
|[Altaic languages](https://translation-commons.github.io/lang-nav/?view=Hierarchy&languageSchema=ISO&searchString=altaic)
|[Japanese languages](https://translation-commons.github.io/lang-nav/?view=Hierarchy&languageSchema=ISO&searchString=Japanese)

src/views/common/ObjectField.tsx
@@ -4,7 +4,8 @@ import { usePageParams } from '../../controls/PageParamsContext';
 import Highlightable from '../../generic/Highlightable';
 import { anyWordStartsWith } from '../../generic/stringUtils';
 import { ObjectData } from '../../types/DataTypes';
-import { SearchableField } from '../../types/PageParamTypes';
+import { LanguageSchema } from '../../types/LanguageTypes';
+import { ObjectType, SearchableField } from '../../types/PageParamTypes';
 
 interface Props {
   object: ObjectData;
@@ -72,5 +73,33 @@ export function getSearchableField(object: ObjectData, field: SearchableField, q
     case SearchableField.NameOrCode:
     case SearchableField.Territory:
       return object.nameDisplay + ' [' + object.codeDisplay + ']';
+  }
+}
+
+export function getObjectPopulation(
+  object: ObjectData,
+  includeDescendents?: boolean,
+  languageSchema?: LanguageSchema,
+): number {
+  switch (object.type) {
+    case ObjectType.Language:
+      return (
+        (object.populationCited ?? 0) +
+        (includeDescendents && languageSchema
+          ? (object.schemaSpecific[languageSchema].populationOfDescendents ?? 0)
+          : 0)
+      );
+    case ObjectType.Locale:
+      return object.populationEstimate;
+    case ObjectType.Census:
+      return object.languageCount;
+    case ObjectType.WritingSystem:
+      return (
+        object.populationUpperBound + (includeDescendents ? object.populationOfDescendents : 0)
+      );
+    case ObjectType.Territory:
+      return object.population;
+    default:
+      return 0; // Default case for other types
   }
 }

Translation-Commons/lang-nav

src/views/common/ObjectField.tsx
@@ -4,7 +4,8 @@ import { usePageParams } from '../../controls/PageParamsContext';
 import Highlightable from '../../generic/Highlightable';
 import { anyWordStartsWith } from '../../generic/stringUtils';
 import { ObjectData } from '../../types/DataTypes';
-import { SearchableField } from '../../types/PageParamTypes';
+import { LanguageSchema } from '../../types/LanguageTypes';
+import { ObjectType, SearchableField } from '../../types/PageParamTypes';
 
 interface Props {
   object: ObjectData;
@@ -72,5 +73,33 @@ export function getSearchableField(object: ObjectData, field: SearchableField, q
     case SearchableField.NameOrCode:
     case SearchableField.Territory:
       return object.nameDisplay + ' [' + object.codeDisplay + ']';
+  }
+}
+
+export function getObjectPopulation(
+  object: ObjectData,
+  includeDescendents?: boolean,
+  languageSchema?: LanguageSchema,
+): number {
+  switch (object.type) {
+    case ObjectType.Language:
+      return (
+        (object.populationCited ?? 0) +
+        (includeDescendents && languageSchema
+          ? (object.schemaSpecific[languageSchema].populationOfDescendents ?? 0)
+          : 0)
+      );
+    case ObjectType.Locale:
+      return object.populationSpeaking;
+    case ObjectType.Census:
+      return object.languageCount;
+    case ObjectType.WritingSystem:
+      return (
+        object.populationUpperBound + (includeDescendents ? object.populationOfDescendents : 0)
+      );
+    case ObjectType.Territory:
+      return object.population;
+    default:
+      return 0; // Default case for other types
   }
 }