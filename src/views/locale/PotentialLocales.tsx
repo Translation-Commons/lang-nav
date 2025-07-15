3f0f3b8c54da3beb961de46f8a3976a2b3f5e574
Conrad Nied <conrad.logos@gmail.com>
2025-07-15 12:10:08 -0700

    Filters: Update style, fix bug

--- src/views/locale/PotentialLocales.tsx
+++ src/views/locale/PotentialLocales.tsx
@@ -9,9 +9,9 @@ import { numberToFixedUnlessSmall } from '../../generic/numberUtils';
 import { CensusData } from '../../types/CensusTypes';
 import {
   BCP47LocaleCode,
+  isTerritoryGroup,
   LocaleData,
   PopulationSourceCategory,
-  TerritoryScope,
 } from '../../types/DataTypes';
 import { LanguageCode, LanguageData } from '../../types/LanguageTypes';
 import { LocaleSeparator, ObjectType, SortBy } from '../../types/PageParamTypes';
@@ -254,10 +254,7 @@ function getPotentialLocales(
       Record<LanguageCode, LocaleData[]>
     >((byLanguage, locale) => {
       const territoryScope = locale.territory?.scope;
-      if (
-        territoryScope === TerritoryScope.Country ||
-        territoryScope === TerritoryScope.Dependency
-      ) {
+      if (isTerritoryGroup(territoryScope)) {
         return byLanguage; // Skip regional locales, censuses are not at the regional level
       }
 
