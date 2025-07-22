bd38215dc712d21d789fd789f4caed2297e6b43f
Conrad Nied <conrad@translationcommons.org>
2025-07-21 22:04:03 -0700

    Filters: Show why there are hidden entries. (#111)
    This change refactors the VisibleItemsMeter, simplifying the code and
    improving the communication about what is filtered out. Now it has 3
    discrete sections: "Showing X of Y results", "Z filtered out", "On Page
    < W > of V". If you hover of the filtered out section it provides a bit
    more context -- I'd like to add more but right now I'm working on
    refactoring the code that will appear there so I think its better to
    start with this and incrementally update.
    
    |Before|After|
    |--|--|
    <img width="677" height="285" alt="स्क्रीनशॉट 2025-07-15, 12 59 53 पर"
    src="https://github.com/user-attachments/assets/5e58e2dc-9e88-4596-876f-7b305e91af0f"
    />|<img width="653" height="293" alt="स्क्रीनशॉट 2025-07-15, 13 01 35
    पर"
    src="https://github.com/user-attachments/assets/94013354-bb98-41dc-9834-d26622e34f46"
    />
    
    And see here how what it looks like 
    <img width="1136" height="318" alt="स्क्रीनशॉट 2025-07-15, 13 15 05 पर"
    src="https://github.com/user-attachments/assets/e6afb9a0-67b2-4274-ba03-b80e2904cac2"
    />

--- src/views/VisibleItemsMeter.tsx
+++ src/views/VisibleItemsMeter.tsx
@@ -1,86 +1,72 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
+import { getScopeFilter, getSubstringFilter } from '../controls/filter';
 import { usePageParams } from '../controls/PageParamsContext';
 import PaginationControls from '../controls/selectors/PaginationControls';
-import { LanguageSchema } from '../types/LanguageTypes';
-import { ObjectType } from '../types/PageParamTypes';
-
-import { getObjectTypeLabelPlural } from './common/getObjectName';
+import Deemphasized from '../generic/Deemphasized';
+import Hoverable from '../generic/Hoverable';
+import { ObjectData } from '../types/DataTypes';
 
 interface Props {
-  nShown: number;
-  nFiltered: number;
-  nOverall: number;
-  objectType?: ObjectType;
+  objects: ObjectData[];
 }
 
-const VisibleItemsMeter: React.FC<Props> = ({ nShown, nFiltered, nOverall, objectType }) => {
+const VisibleItemsMeter: React.FC<Props> = ({ objects }) => {
   const { page, limit } = usePageParams();
+  const substringFilter = getSubstringFilter() ?? (() => true);
+  const scopeFilter = getScopeFilter();
 
+  // Compute amounts
+  const nOverall = objects.length;
   if (nOverall === 0) {
     return 'Data is still loading. If you are waiting awhile there could be an error in the data.';
   }
-  const totalItemsLoaded = (
-    <>
-      {nOverall > nFiltered && <> There are {<strong>{nOverall}</strong>} total</>}{' '}
-      <ObjectTypeLabel manualObjectType={objectType} />.
-    </>
+
+  // nFiltered
+  const nInScope = useMemo(() => objects.filter(scopeFilter).length, [objects, scopeFilter]);
+  const nMatchingSubstring = useMemo(
+    () => objects.filter(scopeFilter).filter(substringFilter).length,
+    [objects, scopeFilter, substringFilter],
   );
+  const nFilteredByScope = nOverall - nInScope;
+  const nFilteredBySubstring = nInScope - nMatchingSubstring;
+  const nFiltered = nMatchingSubstring;
+  const nPages = limit < 1 ? 1 : Math.ceil(nFiltered / limit);
 
-  if (nFiltered === 0) {
-    return (
-      <div style={{ display: 'inline-block' }}>
-        No results found with current filters.{totalItemsLoaded}
-      </div>
-    );
-  }
-
-  if (nFiltered === nOverall) {
-    // Easy case, no filter changing results
-    return (
-      <div style={{ display: 'inline-block' }}>
-        Showing <strong>{nShown}</strong>
-        {nOverall > nShown && <> of {<strong>{nOverall}</strong>}</>}{' '}
-        <ObjectTypeLabel manualObjectType={objectType} />.{' '}
-        <PaginationControls
-          currentPage={page}
-          totalPages={limit < 1 ? 1 : Math.ceil(nFiltered / limit)}
-        />
-      </div>
-    );
-  }
+  // nShown
+  let nShown = limit;
+  if (page > nPages || limit < 1) nShown = 0;
+  if (page === nPages /* last page */) nShown = nFiltered - (nPages - 1) * limit;
 
   return (
-    <div>
-      Showing <strong>{nShown}</strong>
-      {nFiltered > nShown && <> of {<strong>{nFiltered}</strong>}</>} filtered{' '}
-      <ObjectTypeLabel manualObjectType={objectType} />.{nOverall > nFiltered && totalItemsLoaded}{' '}
-      <PaginationControls
-        currentPage={page}
-        totalPages={limit < 1 ? 1 : Math.ceil(nFiltered / limit)}
-      />
+    <div className="VisibleItemsMeter">
+      <div>
+        Showing <strong>{nShown.toLocaleString()}</strong>
+        {nFiltered > nShown && <> of {<strong>{nFiltered.toLocaleString()}</strong>}</>} results.
+      </div>
+      {nOverall > nFiltered && (
+        <Hoverable
+          hoverContent={
+            <>
+              {nFilteredByScope > 0 && <div>Out of scope: {nFilteredByScope.toLocaleString()}</div>}
+              {nFilteredBySubstring > 0 && (
+                <div>Not matching substring: {nFilteredBySubstring.toLocaleString()}</div>
+              )}
+            </>
+          }
+          style={{ textDecoration: 'none' }}
+        >
+          <Deemphasized>{(nOverall - nFiltered).toLocaleString()} filtered out.</Deemphasized>
+        </Hoverable>
+      )}
+      {nPages > 1 && (
+        <div>
+          On <PaginationControls currentPage={page} totalPages={nPages} />
+          of {nPages.toLocaleString()}.
+        </div>
+      )}
     </div>
   );
-};
-
-const ObjectTypeLabel: React.FC<{ manualObjectType?: ObjectType }> = ({ manualObjectType }) => {
-  const { languageSchema, objectType: pageObjectType } = usePageParams();
-  const objectType = manualObjectType ?? pageObjectType;
-
-  if (objectType === ObjectType.Language) {
-    switch (languageSchema) {
-      case LanguageSchema.Glottolog:
-        return 'glottolog languages';
-      case LanguageSchema.Inclusive:
-        return 'languages and language-like entities';
-      case LanguageSchema.ISO:
-        return 'ISO languages';
-      case LanguageSchema.UNESCO:
-      case LanguageSchema.CLDR:
-        break; // fall back to the regular plural (languages)
-    }
-  }
-  return getObjectTypeLabelPlural(objectType);
 };
 
 export default VisibleItemsMeter;
