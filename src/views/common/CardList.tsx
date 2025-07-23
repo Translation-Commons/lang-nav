45996f13a65e9de06329260380ae472c0f1d0537
Conrad Nied <conrad.logos@gmail.com>
2025-07-23 09:58:27 -0700

    UX: Update CardList away from CSS

--- src/views/common/CardList.tsx
+++ src/views/common/CardList.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 import { getScopeFilter, getSliceFunction, getSubstringFilter } from '../../controls/filter';
 import { getSortFunction } from '../../controls/sort';
@@ -24,17 +24,51 @@ function CardList<T extends ObjectData>({ objects, renderCard }: Props<T>) {
   );
 
   return (
-    <div>
-      <div className="CardListDescription">
+    <>
+      <div style={{ marginBottom: '1em' }}>
         <VisibleItemsMeter objects={objects} />
       </div>
-      <div className="CardList">
+      <ResponsiveGrid>
         {objectsVisible.map((object) => (
           <ViewCard key={object.ID}>{renderCard(object)}</ViewCard>
         ))}
-      </div>
-    </div>
+      </ResponsiveGrid>
+    </>
   );
 }
+
+const ResponsiveGrid: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const observer = new ResizeObserver(([entry]) => {
+      if (entry.contentRect) {
+        setWidth(entry.contentRect.width);
+      }
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
+  const nColumns = width > 0 ? Math.floor(width / 300) : 1;
+
+  return (
+    <div
+      ref={containerRef}
+      style={{
+        display: 'grid',
+        gridGap: '1.5em',
+        gridTemplateColumns: `repeat(${nColumns}, 1fr)`,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default CardList;
