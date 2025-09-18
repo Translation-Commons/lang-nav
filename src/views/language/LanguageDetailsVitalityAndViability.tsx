e1a0dd1c001f57c41e4fa181c18bcd841ea7177f
abdulkalam556 <shaikabdulkalamazad1@gmail.com>
2025-09-18 12:25:00 -0700

    feat(language): show raw text + meter on details page

--- src/views/language/LanguageDetailsVitalityAndViability.tsx
+++ src/views/language/LanguageDetailsVitalityAndViability.tsx
@@ -5,6 +5,7 @@ import { CLDRCoverageText, ICUSupportStatus } from '../common/CLDRCoverageInfo';
 import DetailsField from '../common/details/DetailsField';
 import DetailsSection from '../common/details/DetailsSection';
 import ObjectWikipediaInfo from '../common/ObjectWikipediaInfo';
+import LanguageVitalityMeter from './LanguageVitalityMeter';
 
 const LanguageDetailsVitalityAndViability: React.FC<{ lang: LanguageData }> = ({ lang }) => {
   const {
@@ -19,7 +20,12 @@ const LanguageDetailsVitalityAndViability: React.FC<{ lang: LanguageData }> = ({
     <DetailsSection title="Vitality & Viability">
       {vitalityISO && <DetailsField title="ISO Vitality / Status:">{vitalityISO}</DetailsField>}
       {vitalityEth2013 && (
-        <DetailsField title="Ethnologue Vitality (2013):">{vitalityEth2013}</DetailsField>
+        <DetailsField title="Ethnologue Vitality (2013):">
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+            <span>{vitalityEth2013}</span>
+            <LanguageVitalityMeter value={vitalityEth2013} />
+          </div>
+        </DetailsField>
       )}
       {vitalityEth2025 && (
         <DetailsField title="Ethnologue Vitality (2025):">{vitalityEth2025}</DetailsField>
