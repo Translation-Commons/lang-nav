eac6f25e72607574a46b1107f65c196c4ee73d60
Conrad Nied <conrad@translationcommons.org>
2025-09-28 09:32:34 -0700

    Censuses: Add more data imported from data.un.org (#229)
    Adding more data that's been parsed recently.
    
    When adding locales I also added a few provisions to decrease the
    problems when adding locales and messing the number of lines imported.
    That said, we can probably simplify this locale file (will follow up
    with next change to do that).

--- src/data/DataParsing.tsx
+++ src/data/DataParsing.tsx
@@ -15,26 +15,26 @@ export function parseLanguageLine(line: string): LanguageData {
   const nameFull = parts[2];
   const [nameDisplay, nameSubtitle] = separateTitleAndSubtitle(nameFull);
   const populationAdjusted =
-    parts[9] != '' ? Number.parseInt(parts[9].replace(/,/g, '')) : undefined;
+    parts[9] !== '' ? Number.parseInt(parts[9].replace(/,/g, '')) : undefined;
   const populationCited =
-    parts[10] != '' ? Number.parseInt(parts[10].replace(/,/g, '')) : undefined;
+    parts[10] !== '' ? Number.parseInt(parts[10].replace(/,/g, '')) : undefined;
   const code = parts[0];
-  const parentLanguageCode = parts[11] != '' ? parts[11] : undefined;
-  const parentISOCode = parts[11] != '' && parts[11].length <= 3 ? parts[11] : undefined;
-  const parentGlottocode = parts[12] != '' ? parts[12] : undefined;
+  const parentLanguageCode = parts[11] !== '' ? parts[11] : undefined;
+  const parentISOCode = parts[11] !== '' && parts[11].length <= 3 ? parts[11] : undefined;
+  const parentGlottocode = parts[12] !== '' ? parts[12] : undefined;
   const sourceSpecific = {
     All: { code, name: nameDisplay, parentLanguageCode, childLanguages: [] },
     ISO: { code, parentLanguageCode: parentISOCode, childLanguages: [] },
     BCP: { code, parentLanguageCode: parentISOCode, childLanguages: [] },
     UNESCO: { code, name: nameDisplay, parentLanguageCode: parentISOCode, childLanguages: [] },
     Glottolog: {
-      code: parts[1] != '' ? parts[1] : undefined,
+      code: parts[1] !== '' ? parts[1] : undefined,
       parentLanguageCode: parentGlottocode,
       childLanguages: [],
     },
     CLDR: { childLanguages: [] }, // Empty for now
   };
-  const nameEndonym = parts[3] != '' ? parts[3] : undefined;
+  const nameEndonym = parts[3] !== '' ? parts[3] : undefined;
 
   return {
     ...getBaseLanguageData(code, nameDisplay),
@@ -48,26 +48,33 @@ export function parseLanguageLine(line: string): LanguageData {
     names: [nameDisplay, nameSubtitle, nameEndonym].filter((s) => s != null),
 
     vitalityISO: undefined, // Added by ISO import
-    vitalityEth2013: parts[6] != '' ? parts[6] : undefined,
-    vitalityEth2025: parts[7] != '' ? parts[7] : undefined,
-    digitalSupport: parts[8] != '' ? parts[8] : undefined,
-    viabilityConfidence: parts[13] != '' ? parts[13] : 'No',
-    viabilityExplanation: parts[14] != '' ? parts[14] : undefined,
+    vitalityEth2013: parts[6] !== '' ? parts[6] : undefined,
+    vitalityEth2025: parts[7] !== '' ? parts[7] : undefined,
+    digitalSupport: parts[8] !== '' ? parts[8] : undefined,
+    viabilityConfidence: parts[13] !== '' ? parts[13] : 'No',
+    viabilityExplanation: parts[14] !== '' ? parts[14] : undefined,
 
     populationAdjusted,
     populationCited,
 
     modality: parts[4] !== '' ? (parts[4] as LanguageModality) : undefined,
-    primaryScriptCode: parts[5] != '' ? parts[5] : undefined,
+    primaryScriptCode: parts[5] !== '' ? parts[5] : undefined,
 
     sourceSpecific,
   };
 }
 
-export function parseLocaleLine(line: string): LocaleData {
+export function parseLocaleLine(line: string): LocaleData | null {
   const parts = line.split('\t');
-  const nameEndonym = parts[2] != '' ? parts[2] : undefined;
-  const variantTagCode = parts[6] != '' ? parts[6].toLowerCase() : undefined;
+  if (parts.length === 1 && parts[0] === '') {
+    // Empty line, ignore
+    return null;
+  } else if (parts.length !== 10) {
+    console.error(`Locale line not the right length, ${parts.length} parts: ${line}`);
+    return null;
+  }
+  const nameEndonym = parts[2] !== '' ? parts[2] : undefined;
+  const variantTagCode = parts[6] !== '' ? parts[6].toLowerCase() : undefined;
 
   return {
     type: ObjectType.Locale,
@@ -76,15 +83,15 @@ export function parseLocaleLine(line: string): LocaleData {
     localeSource: 'regularInput',
 
     nameDisplay: parts[1],
-    nameEndonym: parts[2] != '' ? parts[2] : undefined,
+    nameEndonym: parts[2] !== '' ? parts[2] : undefined,
     names: [parts[1], nameEndonym].filter((s) => s != null),
     languageCode: parts[3],
     territoryCode: parts[4],
-    explicitScriptCode: parts[5] != '' ? parts[5] : undefined,
+    explicitScriptCode: parts[5] !== '' ? parts[5] : undefined,
     variantTagCode,
     populationSource: parts[7] as PopulationSourceCategory,
     populationSpeaking: Number.parseInt(parts[8]?.replace(/,/g, '')),
-    officialStatus: parts[9] != '' ? (parts[9] as OfficialStatus) : undefined,
+    officialStatus: parts[9] !== '' ? (parts[9] as OfficialStatus) : undefined,
 
     censusRecords: [], // Populated later
   };
@@ -92,7 +99,7 @@ export function parseLocaleLine(line: string): LocaleData {
 
 export function parseWritingSystem(line: string): WritingSystemData {
   const parts = line.split('\t');
-  const nameEndonym = parts[3] != '' ? parts[3] : undefined;
+  const nameEndonym = parts[3] !== '' ? parts[3] : undefined;
   return {
     type: ObjectType.WritingSystem,
 
@@ -104,13 +111,13 @@ export function parseWritingSystem(line: string): WritingSystemData {
     nameFull: parts[2],
     nameEndonym,
     names: [parts[1], parts[2], nameEndonym].filter((s) => s != null),
-    unicodeVersion: parts[4] != '' ? parseFloat(parts[4]) : null,
-    sample: parts[5] != '' ? parts[5] : null,
+    unicodeVersion: parts[4] !== '' ? parseFloat(parts[4]) : null,
+    sample: parts[5] !== '' ? parts[5] : null,
     rightToLeft: parts[6] === 'Yes' ? true : parts[6] === 'no' ? false : null,
-    primaryLanguageCode: parts[7] != '' ? parts[7] : null,
-    territoryOfOriginCode: parts[8] != '' ? parts[8] : null,
-    parentWritingSystemCode: parts[9] != '' ? parts[9] : null,
-    containsWritingSystemsCodes: parts[10] != '' ? parts[10].split(', ') : [],
+    primaryLanguageCode: parts[7] !== '' ? parts[7] : null,
+    territoryOfOriginCode: parts[8] !== '' ? parts[8] : null,
+    parentWritingSystemCode: parts[9] !== '' ? parts[9] : null,
+    containsWritingSystemsCodes: parts[10] !== '' ? parts[10].split(', ') : [],
 
     // Derived when combining other data
     populationUpperBound: 0,
