e3b66c78a8942a54eb339047990f55bcc0a4ce1a
Mark Chandra Chowdhary <mcc.westend@gmail.com>
2025-12-13 17:06:46 +0100

    Refactor: Consolidate scale-by logic into getScaleBysApplicableToObjectType for cleaner code

--- src/features/transforms/scales/ScaleBySelector.tsx
+++ src/features/transforms/scales/ScaleBySelector.tsx
@@ -1,40 +1,17 @@
 import React from 'react';
 
-import { ObjectType } from '@features/params/PageParamTypes';
 import Selector from '@features/params/ui/Selector';
 import { SelectorDisplay, useSelectorDisplay } from '@features/params/ui/SelectorDisplayContext';
 import usePageParams from '@features/params/usePageParams';
-import { getSortBysApplicableToObjectType } from '@features/transforms/sorting/sort';
-import { SortBy } from '@features/transforms/sorting/SortTypes';
 
-import { ScaleBy } from './ScaleTypes';
+import { getScaleBysApplicableToObjectType, ScaleBy } from './ScaleTypes';
 
 const ScaleBySelector: React.FC = () => {
   const { scaleBy, updatePageParams, objectType } = usePageParams();
   const { display } = useSelectorDisplay();
 
-  const applicableScaleBys = getSortBysApplicableToObjectType(objectType);
-
-  // Limit scale-by choices to only the fields we want to support for scaling
-  const allowedScaleBys: SortBy[] = [
-    SortBy.Population,
-    SortBy.PopulationAttested,
-    SortBy.CountOfLanguages,
-    SortBy.CountOfTerritories,
-  ];
-
-  // Filter allowed options by what is applicable to the current object type
-  let filteredAllowed = allowedScaleBys.filter((s) => applicableScaleBys.includes(s));
-
-  // Exclude count-of-languages when mapping Languages, and exclude count-of-territories when mapping Territories
-  if (objectType === ObjectType.Language) {
-    filteredAllowed = filteredAllowed.filter((s) => s !== SortBy.CountOfLanguages);
-  }
-  if (objectType === ObjectType.Territory) {
-    filteredAllowed = filteredAllowed.filter((s) => s !== SortBy.CountOfTerritories);
-  }
-
-  const scaleByOptions: ScaleBy[] = ['None', ...filteredAllowed];
+  const applicableScaleBys = getScaleBysApplicableToObjectType(objectType);
+  const scaleByOptions: ScaleBy[] = ['None', ...applicableScaleBys];
 
   return (
     <Selector<ScaleBy>
--- src/features/transforms/scales/ScaleTypes.ts
+++ src/features/transforms/scales/ScaleTypes.ts
@@ -1,4 +1,37 @@
+import { ObjectType } from '@features/params/PageParamTypes';
+
 import { SortBy } from '../sorting/SortTypes';
 
 // ScaleBy can be any SortBy value or 'None'
 export type ScaleBy = SortBy | 'None';
+
+/**
+ * Get the scale-by options applicable to a given object type.
+ * Returns a filtered list of allowed scale-by fields based on the object type.
+ */
+export function getScaleBysApplicableToObjectType(objectType: ObjectType): SortBy[] {
+  // Base allowed scale-by fields (all support scaling)
+  const allowedScaleBys: SortBy[] = [
+    SortBy.Population,
+    SortBy.PopulationAttested,
+    SortBy.Area,
+    SortBy.CountOfLanguages,
+    SortBy.CountOfTerritories,
+  ];
+
+  let filteredScaleBys = [...allowedScaleBys];
+
+  // Exclude count-of-languages and area when mapping Languages
+  if (objectType === ObjectType.Language) {
+    filteredScaleBys = filteredScaleBys.filter(
+      (s) => s !== SortBy.CountOfLanguages && s !== SortBy.Area,
+    );
+  }
+
+  // Exclude count-of-territories when mapping Territories
+  if (objectType === ObjectType.Territory) {
+    filteredScaleBys = filteredScaleBys.filter((s) => s !== SortBy.CountOfTerritories);
+  }
+
+  return filteredScaleBys;
+}
