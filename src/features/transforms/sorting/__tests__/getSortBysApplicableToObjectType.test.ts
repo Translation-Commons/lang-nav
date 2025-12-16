15cc544c463346fc46422b7a19aebb77193c0d82
Conrad Nied <conrad@translationcommons.org>
2025-12-15 20:54:20 -0800

    Sorting: Review getApplicableSortBy values (#332)
    While I was talking with @cchandram about ScaleBy values and
    re-organizing how the "getApplicable..." methods work, I wanted to try
    poking holes in `getSortBysApplicableToObjectType`.
    
    1) We can set a few common sortBys for all objects: Name, Code &
    Population. Unfortunately not object types support endonyms since we
    don't have data for endonyms for Variant Tags or Censuses.
    2) Added a test for the function -- most interestingly revealing the
    objects we had truthy values for that weren't listed in the SortBy.
    3) Added some more territory names from my previous change adding
    territory endonyms -- oof I wanted to add this to that change but I
    committed them to the wrong branch.

---
+++ src/features/transforms/sorting/__tests__/getSortBysApplicableToObjectType.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it } from 'vitest';
+
+import { getFullyInstantiatedMockedObjects } from '@features/__tests__/MockObjects';
+import { ObjectType } from '@features/params/PageParamTypes';
+import { getSortField } from '@features/transforms/fields/getField';
+
+import getSortBysApplicableToObjectType from '../getSortBysApplicableToObjectType';
+import { SortBy } from '../SortTypes';
+
+describe('getSortBysApplicableToObjectType', () => {
+  it('should not return duplicate SortBy values for any ObjectType', () => {
+    Object.values(ObjectType).forEach((objectType) => {
+      const sortBys = getSortBysApplicableToObjectType(objectType);
+      const uniqueSortBys = new Set(sortBys);
+      expect(uniqueSortBys.size).toBe(sortBys.length);
+    });
+  });
+
+  it('Check that all possible SortBys are returned for each object type. Literally, if a sortBy is not returned by getSortBysApplicableToObjectType, then getSortField should not return a truthy value for it.', () => {
+    const mockedObjects = getFullyInstantiatedMockedObjects();
+
+    Object.values(ObjectType).forEach((objectType) => {
+      const objectsInType = Object.values(mockedObjects).filter((obj) => obj.type === objectType);
+      const sortBysForType = getSortBysApplicableToObjectType(objectType);
+
+      Object.values(SortBy).forEach((sortBy) => {
+        objectsInType.forEach((obj) => {
+          const sortField = getSortField(obj, sortBy);
+          if (!sortBysForType.includes(sortBy)) {
+            // The value is not supposed to be applicable
+            expect(
+              sortField,
+              `ObjectType (${objectType}) shouldn't be sorted by ${sortBy} but it has a getSortField value so it should be sortable. Failed on object: ${obj.nameDisplay} [${obj.ID}]`,
+            ).toBeFalsy();
+          }
+        });
+      });
+    });
+  });
+});
--- src/features/transforms/sorting/getSortBysApplicableToObjectType.ts
+++ src/features/transforms/sorting/getSortBysApplicableToObjectType.ts
@@ -2,18 +2,22 @@ import { ObjectType } from '@features/params/PageParamTypes';
 
 import { SortBy } from './SortTypes';
 
+const COMMON_SORT_BYS = [SortBy.Code, SortBy.Name, SortBy.Population];
+
 /** Not necessarily exhaustive, just the ones that will appear in the sidebar */
 export default function getSortBysApplicableToObjectType(objectType: ObjectType): SortBy[] {
+  return [...COMMON_SORT_BYS, ...getSortBysSpecificToObjectType(objectType)];
+}
+
+function getSortBysSpecificToObjectType(objectType: ObjectType): SortBy[] {
   switch (objectType) {
     case ObjectType.Locale:
       return [
-        SortBy.Code,
-        SortBy.Name,
         SortBy.Endonym,
-        SortBy.Population,
         SortBy.PopulationDirectlySourced,
         SortBy.Literacy,
         SortBy.PercentOfOverallLanguageSpeakers,
+        SortBy.PercentOfTerritoryPopulation,
         SortBy.CountOfLanguages,
         SortBy.Language,
         SortBy.WritingSystem,
@@ -21,28 +25,32 @@ export default function getSortBysApplicableToObjectType(objectType: ObjectType)
       ];
     case ObjectType.Territory:
       return [
-        SortBy.Code,
-        SortBy.Name,
-        SortBy.Population,
+        SortBy.Endonym,
         SortBy.Literacy,
         SortBy.CountOfLanguages,
         SortBy.CountOfTerritories,
         SortBy.Latitude,
         SortBy.Longitude,
         SortBy.Area,
+        SortBy.Language,
+        SortBy.WritingSystem,
+        SortBy.Territory, // Equivalent to DisplayName for territories
+        SortBy.PopulationDirectlySourced,
+        SortBy.PercentOfTerritoryPopulation,
+        SortBy.PopulationPercentInBiggestDescendantLanguage,
       ];
     case ObjectType.Language:
       return [
-        SortBy.Code,
-        SortBy.Name,
         SortBy.Endonym,
-        SortBy.Population,
         SortBy.Literacy,
         SortBy.CountOfTerritories,
         SortBy.CountOfLanguages,
+        SortBy.Language, // Equivalent to DisplayName for languages
         SortBy.WritingSystem,
         SortBy.Territory,
         SortBy.PopulationDirectlySourced,
+        SortBy.PopulationOfDescendants,
+        SortBy.PercentOfOverallLanguageSpeakers,
         SortBy.VitalityMetascore,
         SortBy.ISOStatus,
         SortBy.VitalityEthnologue2013,
@@ -53,24 +61,22 @@ export default function getSortBysApplicableToObjectType(objectType: ObjectType)
     case ObjectType.Census:
       return [
         SortBy.Date,
-        SortBy.Code,
-        SortBy.Name,
-        SortBy.Population,
+        SortBy.PopulationDirectlySourced,
+        SortBy.PercentOfTerritoryPopulation,
         SortBy.Territory,
         SortBy.CountOfLanguages,
       ];
     case ObjectType.WritingSystem:
       return [
-        SortBy.Code,
-        SortBy.Name,
         SortBy.Endonym,
-        SortBy.Population,
         // SortBy.Literacy, Data not available yet
         SortBy.Language,
         SortBy.CountOfLanguages,
         SortBy.PopulationOfDescendants,
+        SortBy.Territory,
+        SortBy.WritingSystem, // Equivalent to DisplayName for writing systems
       ];
     case ObjectType.VariantTag:
-      return [SortBy.Date, SortBy.Code, SortBy.Name, SortBy.Population, SortBy.CountOfLanguages];
+      return [SortBy.Date, SortBy.CountOfLanguages, SortBy.Language];
   }
 }
