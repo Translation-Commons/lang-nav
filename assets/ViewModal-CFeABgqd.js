37888035c640e963cfee2633090d72e0d10fa2b5
conradarcturus <conradarcturus@users.noreply.github.com>
2026-01-03 18:13:07 +0000

    deploy: 29795255cff656206089f4a2513c566973093f56

--- assets/ViewModal-CFeABgqd.js
+++ assets/ViewModal-CFeABgqd.js
@@ -1,4 +1,4 @@
-import{l as m,k as p,r as h,V as o,j as e}from"./index-C5fIqoVo.js";import{d as j,b as x,a2 as y,a3 as v,O as u}from"./ViewDetails-BWA3mTMj.js";import{K as k,r as l,X as b}from"./BaseColorBar-DWl7cpFH.js";import"./useDataContext-Dwu6XEex.js";import"./CLDRTypes-BZpcyur0.js";import"./VitalityTypes-CbVUEUAD.js";/**
+import{l as m,k as p,r as h,V as o,j as e}from"./index-DKq4Gxk2.js";import{d as j,b as x,a3 as y,a4 as v,O as u}from"./ViewDetails-C0azmxBO.js";import{K as k,r as l,X as b}from"./BaseColorBar-DKCf2am3.js";import"./useDataContext-BsV0vlqP.js";import"./CLDRTypes-CkKbgIpX.js";import"./VitalityTypes-CbVUEUAD.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
