8355bbb76d0706ae1d099b5729f5f72e8a654ba7
mchen1107 <chen.xum@northeastern.edu>
2026-02-19 23:49:56 -0800

    refactor: convert VariantTagCard to CardField layout

--- src/entities/varianttag/VariantTagCard.tsx
+++ src/entities/varianttag/VariantTagCard.tsx
@@ -1,14 +1,14 @@
-// src/views/varianttag/VariantTagCard.tsx
-
+import { LanguagesIcon, TagIcon } from 'lucide-react';
 import React from 'react';
 
 import HoverableObjectName from '@features/layers/hovercard/HoverableObjectName';
 
 import { VariantTagData } from '@entities/types/DataTypes';
-import ObjectSubtitle from '@entities/ui/ObjectSubtitle';
 import ObjectTitle from '@entities/ui/ObjectTitle';
 
+import CardField from '@shared/containers/CardField';
 import CommaSeparated from '@shared/ui/CommaSeparated';
+import Deemphasized from '@shared/ui/Deemphasized';
 
 interface Props {
   data: VariantTagData;
@@ -21,24 +21,31 @@ const VariantTagCard: React.FC<Props> = ({ data }) => {
     <div>
       <h3>
         <ObjectTitle object={data} />
-        <ObjectSubtitle object={data} />
       </h3>
 
-      <div>
-        <label>Name:</label>
-        {nameDisplay}
-      </div>
+      <CardField
+        title="Name"
+        icon={TagIcon}
+        description="The name used to identify this variant tag."
+      >
+        {nameDisplay ?? <Deemphasized>Unknown</Deemphasized>}
+      </CardField>
 
-      {languages && Object.values(languages).length > 0 && (
-        <div>
-          <label>Languages:</label>
+      <CardField
+        title="Languages"
+        icon={LanguagesIcon}
+        description="Languages that use this variant tag."
+      >
+        {languages && Object.values(languages).length > 0 ? (
           <CommaSeparated>
             {Object.values(languages).map((lang) => (
               <HoverableObjectName key={lang.ID} object={lang} />
             ))}
           </CommaSeparated>
-        </div>
-      )}
+        ) : (
+          <Deemphasized>Unknown</Deemphasized>
+        )}
+      </CardField>
     </div>
   );
 };
