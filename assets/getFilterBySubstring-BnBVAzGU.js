7019604b871f9fdfd8d43e4d6eaf205a0412c48b
mchen11077 <mchen11077@users.noreply.github.com>
2026-02-23 03:21:57 +0000

    deploy: b9ec5d7ac27bbc90710ff5aaf949800aef3ddfa2

---
+++ assets/getFilterBySubstring-BnBVAzGU.js
@@ -0,0 +1,1 @@
+import{H as e}from"./index-Bcbs_t3h.js";import{g as s}from"./filterByConnections-bF2BnOyq.js";function a(){const{searchBy:t,searchString:r}=e();return r==""?()=>!0:s(r,t)}export{a as g};
