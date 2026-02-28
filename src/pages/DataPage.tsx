e38b3d65da1aa12182d7b57c20a2aa6ab1e0cf5e
mchen1107 <chen.xum@northeastern.edu>
2026-02-27 16:22:46 -0800

    refactor: move DataProvider to app level with deferred loading

--- src/pages/DataPage.tsx
+++ src/pages/DataPage.tsx
@@ -5,29 +5,23 @@ import Loading from '@widgets/Loading';
 
 import HoverCardProvider from '@features/layers/hovercard/HoverCardProvider';
 
-const PageParamsProvider = React.lazy(() => import('@features/params/PageParamsProvider'));
-const DataProvider = React.lazy(() => import('@features/data/context/DataProvider'));
 const DataPageBody = React.lazy(() => import('./DataPageBody'));
 const OptionsPanel = React.lazy(() => import('@widgets/controls/OptionsPanel'));
 const ViewModal = React.lazy(() => import('@features/layers/modal/ViewModal'));
 
 const DataPage: React.FC = () => {
-  /* DataProvider and many other data components have more lines of code so they are loaded lazily */
+  /* Many data components have more lines of code so they are loaded lazily */
   return (
     <Suspense fallback={<Loading />}>
-      <PageParamsProvider>
-        <HoverCardProvider>
-          {/* HoverCardProvider is re-declared so it has access to page parameters, there may be a better way to organize it */}
-          <DataProvider>
-            <div style={{ display: 'flex', height: '100vh' }}>
-              <OptionsPanel />
-              <DataPageBody />
-              <DetailsPanel />
-            </div>
-            <ViewModal />
-          </DataProvider>
-        </HoverCardProvider>
-      </PageParamsProvider>
+      <HoverCardProvider>
+        {/* HoverCardProvider is re-declared so it has access to page parameters, there may be a better way to organize it */}
+        <div style={{ display: 'flex', height: '100vh' }}>
+          <OptionsPanel />
+          <DataPageBody />
+          <DetailsPanel />
+        </div>
+        <ViewModal />
+      </HoverCardProvider>
     </Suspense>
   );
 };
