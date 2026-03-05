0dc2f2885f9b9be42df6aaaa3501d9ca0b9fa444
Conrad Nied <conrad.logos@gmail.com>
2026-03-04 19:59:33 -0800

    Color: Update rainbow gradient, add scattered gradient

--- src/features/transforms/coloring/BaseColorBar.tsx
+++ src/features/transforms/coloring/BaseColorBar.tsx
@@ -1,14 +1,20 @@
 import { ColorGradient } from './ColorTypes';
 import { getColorGradientFunction } from './getColorGradientFunction';
 
-function BaseColorBar({ colorGradient }: { colorGradient: ColorGradient }) {
+function BaseColorBar({
+  colorGradient,
+  renormalize,
+}: {
+  colorGradient: ColorGradient;
+  renormalize?: (value: number) => number;
+}) {
   const colorFunc = getColorGradientFunction(colorGradient);
   return [...Array(100).keys()].map((i) => {
     return (
       <div
         key={i}
         style={{
-          backgroundColor: colorFunc(i / 99),
+          backgroundColor: colorFunc(renormalize ? renormalize(i / 99) : i / 99),
           width: '1%',
           height: '100%',
           display: 'inline-block',
--- src/features/transforms/coloring/getColorGradientFunction.tsx
+++ src/features/transforms/coloring/getColorGradientFunction.tsx
@@ -1,3 +1,5 @@
+import enforceExhaustiveSwitch from '@shared/lib/enforceExhaustiveness';
+
 import { ColorGradient } from './ColorTypes';
 
 /**
@@ -21,9 +23,19 @@ export function getColorGradientFunction(colorGradient: ColorGradient): (value: 
         return `hsl(${value * 120}, 80%, ${80 - Math.abs(value - 0.5) * 40}%)`;
       };
     case ColorGradient.HueRainbowBlueToRed:
-      // hsl(240, 80%, 60%) -> hsl(180, 80%, 60%) -> hsl(120, 80%, 60%) -> hsl(60, 80%, 60%) -> hsl(0, 80%, 60%)
+      // hsl(240, 90%, 60%) -> hsl(180, 80%, 60%) -> hsl(120, 90%, 60%) -> hsl(60, 90%, 60%) -> hsl(0, 90%, 60%)
+      // No purples (hues between 240 and 360) because then high & low values would look similar
       return (value: number) => {
-        return `hsl(${240 - value * 240}, 80%, 60%)`;
+        return `hsl(${240 - value * 240}, 90%, 60%)`;
+      };
+    case ColorGradient.OklabRainbowBlueToRed:
+      // OKLCH interpolation for more even perceptual spacing
+      // Less greens, more oranges, still no purples (avoiding a loop)
+      return (value: number) => {
+        const lightness = 80 - Math.sin(value * Math.PI * 2 + 1) * 20; // Vary lightness to add more distinction between colors
+        const chroma = 60;
+        const hue = 270 - value * 270;
+        return `oklch(${lightness}% ${chroma}% ${hue})`;
       };
     case ColorGradient.SequentialBlue:
       // Same blue as the primary button color
@@ -50,9 +62,33 @@ export function getColorGradientFunction(colorGradient: ColorGradient): (value: 
     case ColorGradient.HueRainbowRedToBlue:
       return (value: number) =>
         getColorGradientFunction(ColorGradient.HueRainbowBlueToRed)(1 - value);
+    case ColorGradient.OklabRainbowRedToBlue:
+      return (value: number) =>
+        getColorGradientFunction(ColorGradient.OklabRainbowBlueToRed)(1 - value);
     case ColorGradient.SequentialBlueReverse:
       return (value: number) => getColorGradientFunction(ColorGradient.SequentialBlue)(1 - value);
     case ColorGradient.SequentialOrangeReverse:
       return (value: number) => getColorGradientFunction(ColorGradient.SequentialOrange)(1 - value);
+
+    // Categorical
+    case ColorGradient.ScatteredOklab:
+      return (value: number) => {
+        const lightness = 70 + Math.sin(value * Math.PI * 37) * 20;
+        const chroma = 50 + Math.sin(value * Math.PI * 41) * 30; // Vary chroma in a pseudo-random way based on the value, using a sine function for smooth variation
+        const hue = (value * 360 * 137.508) % 360;
+        return `oklch(${lightness}% ${chroma}% ${hue})`;
+      };
+    case ColorGradient.ScatteredRGB:
+      return (value: number) => {
+        // Multiple the normalized value by a prime number and take the fractional part to
+        // get a pseudo-random but deterministic value. Slightly modified to make it brighter.
+
+        const red = ((value * 31) % 1 ** 0.5) + 0.2;
+        const green = ((value * 37) % 1 ** 0.5) + 0.2;
+        const blue = ((value * 41) % 1 ** 0.5) + 0.2;
+        return `rgb(${red * 255}, ${green * 255}, ${blue * 255})`;
+      };
+    default:
+      enforceExhaustiveSwitch(colorGradient);
   }
 }
