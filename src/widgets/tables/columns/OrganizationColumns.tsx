2150df038d71140d035d2c24d6b78ff04c996957
Conrad Nied <conrad.logos@gmail.com>
2026-05-06 22:23:40 -0700

    Update organizations

--- src/widgets/tables/columns/OrganizationColumns.tsx
+++ src/widgets/tables/columns/OrganizationColumns.tsx
@@ -1,20 +1,23 @@
 import HoverableEnumeration from '@features/layers/hovercard/HoverableEnumeration';
 import HoverableObjectName from '@features/layers/hovercard/HoverableObjectName';
-import { CodeColumn, NameColumn } from '@features/table/CommonColumns';
+import { CodeColumn, EndonymColumn, NameColumn } from '@features/table/CommonColumns';
 import TableColumn from '@features/table/TableColumn';
 import Field from '@features/transforms/fields/Field';
 
 import { OrganizationData } from '@entities/org/OrganizationTypes';
 
 import Deemphasized from '@shared/ui/Deemphasized';
+import ExternalLink from '@shared/ui/ExternalLink';
 
 function getOrganizationColumns(): TableColumn<OrganizationData>[] {
   return [
     CodeColumn,
     NameColumn,
+    EndonymColumn,
     {
       key: 'Headquarters',
       render: (object) => <HoverableObjectName object={object.headquarters} />,
+      exportValue: (object) => object.headquarters?.ID ?? '',
       field: Field.Territory,
     },
     {
@@ -31,6 +34,11 @@ function getOrganizationColumns(): TableColumn<OrganizationData>[] {
           <Deemphasized>n/a</Deemphasized>
         ),
       field: Field.CountOfCensuses,
+    },
+    {
+      key: 'URL',
+      render: (object) => object.url && <ExternalLink href={object.url} />,
+      exportValue: (object) => object.url ?? '',
     },
   ];
 }
