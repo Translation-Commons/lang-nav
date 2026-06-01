417b0256f41223de4a5db96914c634c9401ffed0
hdprajwal <hdprajwal@users.noreply.github.com>
2026-06-01 14:46:55 +0000

    deploy: 04de0b518b0287fc248f9a8accc73c27f5c71a00

---
+++ assets/TableOfAllCensuses-rEfhZG_3.js
@@ -0,0 +1,1 @@
+import{aj as r,r as t,o as n}from"./index-BeqYffmQ.js";import{I as a,T as c}from"./TableID-QZQoS6Rs.js";import{g as l}from"./CensusColumns-DxzhGkhc.js";const p=({organization:s})=>{const{censuses:e}=r(),u=t.useMemo(()=>l(),[]),o=t.useMemo(()=>s?s.censuses??[]:Object.values(e),[s,e]);return n.jsx(a,{tableID:c.Censuses,objects:o,columns:u})};export{p as T};
