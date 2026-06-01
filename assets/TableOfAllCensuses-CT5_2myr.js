fc4a2122446e8ec96e3559842fb2a783a56e897b
conradarcturus <conradarcturus@users.noreply.github.com>
2026-06-01 16:42:18 +0000

    deploy: 29f7482751a7e27bc15179feb95093cef3af5379

---
+++ assets/TableOfAllCensuses-CT5_2myr.js
@@ -0,0 +1,1 @@
+import{aj as r,r as t,o as n}from"./index-0c-1hUlX.js";import{I as a,T as c}from"./TableID-BbXE6Pec.js";import{g as l}from"./CensusColumns-BcnsmDKa.js";const p=({organization:s})=>{const{censuses:e}=r(),u=t.useMemo(()=>l(),[]),o=t.useMemo(()=>s?s.censuses??[]:Object.values(e),[s,e]);return n.jsx(a,{tableID:c.Censuses,objects:o,columns:u})};export{p as T};
