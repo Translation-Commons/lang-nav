f6ee764cfe1cca65bc3e461cbd36f7e785010b37
whwan4570 <whwan4570@users.noreply.github.com>
2026-06-27 04:11:14 +0000

    deploy: 3a2f571349f71305881857fa1a2daacd3c548c6f

---
+++ assets/TableOfAllCensuses-B_ICKAlL.js
@@ -0,0 +1,1 @@
+import{al as r,r as t,o}from"./index-9e_a8cFX.js";import{I as a,T as l}from"./TableID-DjB9NDG1.js";import{g as c}from"./CensusColumns-CzIAktZL.js";const C=({organization:s})=>{const{censuses:e}=r(),n=t.useMemo(()=>c(),[]),u=t.useMemo(()=>s?s.censuses??[]:Object.values(e),[s,e]);return o.jsx(a,{tableID:l.Censuses,entities:u,columns:n})};export{C as T};
