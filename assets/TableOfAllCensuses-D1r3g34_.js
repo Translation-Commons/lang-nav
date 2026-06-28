400407818d52b56a25d3485a24e3998367811f8e
dhanush-kannan-dk <dhanush-kannan-dk@users.noreply.github.com>
2026-06-28 17:11:18 +0000

    deploy: e1bb54ce0cbc6fd5cf4b6133a526a700238e0ab6

--- assets/TableOfAllCensuses-D1r3g34_.js
+++
@@ -1,1 +0,0 @@
-import{al as r,r as t,o}from"./index-DalAa6Ai.js";import{I as a,T as l}from"./TableID-Ci1IT3eg.js";import{g as c}from"./CensusColumns-DzIt6tk_.js";const C=({organization:s})=>{const{censuses:e}=r(),n=t.useMemo(()=>c(),[]),u=t.useMemo(()=>s?s.censuses??[]:Object.values(e),[s,e]);return o.jsx(a,{tableID:l.Censuses,entities:u,columns:n})};export{C as T};
