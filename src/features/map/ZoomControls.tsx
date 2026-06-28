cf007f71ee0101effe908cd60489b52db2574621
Dhanush Kannan <dhanush.kannan.dk007@gmail.com>
2026-06-28 22:52:44 +0530

    fix: scale zoom control icon size based on mapContainerWidth

--- src/features/map/ZoomControls.tsx
+++ src/features/map/ZoomControls.tsx
@@ -8,11 +8,18 @@ type Props = {
   zoomIn: () => void;
   zoomOut: () => void;
   resetTransform: () => void;
+  containerWidth?: number;
 };
 
-const ZoomControls: React.FC<Props> = ({ zoomIn, zoomOut, resetTransform }) => {
+const ZoomControls: React.FC<Props> = ({
+  zoomIn,
+  zoomOut,
+  resetTransform,
+  containerWidth = 800,
+}) => {
+  const fontSize = containerWidth < 500 ? '0.7em' : containerWidth < 700 ? '0.85em' : '1em';
   return (
-    <div style={containerStyle}>
+    <div style={{ ...containerStyle, fontSize }}>
       <HoverableIcon onClick={zoomIn} description="Zoom in" Icon={ZoomInIcon} />
       <HoverableIcon onClick={zoomOut} description="Zoom out" Icon={ZoomOutIcon} />
       <HoverableIcon onClick={resetTransform} description="Reset" Icon={Maximize2Icon} />
