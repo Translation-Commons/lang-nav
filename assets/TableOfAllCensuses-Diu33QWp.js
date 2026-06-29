5a64ba5c9998507a50a6fb490b344b4ecaeda55e
kevinmdd <kevinmdd@users.noreply.github.com>
2026-06-29 00:55:18 +0000

    deploy: 044945be259291f75211f54b7473418b02735cb2

--- assets/TableOfAllCensuses-Diu33QWp.js
+++
@@ -1,1 +0,0 @@
-import{al as r,r as t,o}from"./index-B36krlu1.js";import{I as a,T as l}from"./TableID-_NJD8Yfb.js";import{g as c}from"./CensusColumns-DDNosSqE.js";const C=({organization:s})=>{const{censuses:e}=r(),n=t.useMemo(()=>c(),[]),u=t.useMemo(()=>s?s.censuses??[]:Object.values(e),[s,e]);return o.jsx(a,{tableID:l.Censuses,entities:u,columns:n})};export{C as T};
