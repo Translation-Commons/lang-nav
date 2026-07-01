c44fbf10a756936e6d36340f664b863b7a65c3c4
kevinmdd <151257451+kevinmdd@users.noreply.github.com>
2026-07-01 05:22:30 +0000

    Style: auto-fix with prettier and eslint

--- src/widgets/controls/selectors/ClearAllPinsButton.tsx
+++ src/widgets/controls/selectors/ClearAllPinsButton.tsx
@@ -5,26 +5,29 @@ import SelectorLabel from '@features/params/ui/SelectorLabel';
 import usePageParams from '@features/params/usePageParams';
 
 const ClearAllPinsButton: React.FC = () => {
-    const { pinned, updatePageParams } = usePageParams();
+  const { pinned, updatePageParams } = usePageParams();
 
-    const onClearClick = () => { updatePageParams({ pinned: [] }); };
+  const onClearClick = () => {
+    updatePageParams({ pinned: [] });
+  };
 
-    return (
-        pinned.length > 0 && (
-            <div className="selector" style={{ display: 'flex', alignItems: 'center' }}>
-                <SelectorLabel
-                    label="Clear All Pins"
-                    description="Remove all pinned cards from the page."
-                />
-                <button
-                    aria-label="Clear all pinned cards"
-                    className="selected ClearAllPinsButton-button"
-                    onClick={onClearClick}
-                >
-                    <BanIcon className="ClearAllPinsButton-iconBan" size="1em" />
-                </button >
-            </div>)
-    );
+  return (
+    pinned.length > 0 && (
+      <div className="selector" style={{ display: 'flex', alignItems: 'center' }}>
+        <SelectorLabel
+          label="Clear All Pins"
+          description="Remove all pinned cards from the page."
+        />
+        <button
+          aria-label="Clear all pinned cards"
+          className="selected ClearAllPinsButton-button"
+          onClick={onClearClick}
+        >
+          <BanIcon className="ClearAllPinsButton-iconBan" size="1em" />
+        </button>
+      </div>
+    )
+  );
 };
 
 export default ClearAllPinsButton;
