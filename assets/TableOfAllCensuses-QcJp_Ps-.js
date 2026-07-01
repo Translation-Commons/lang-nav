28fa1ca268ffcf111e090fc226a932a69704cac4
conradarcturus <conradarcturus@users.noreply.github.com>
2026-07-01 14:15:54 +0000

    deploy: 30aef6353fb0dc6923e509161e6afef726e18504

---
+++ assets/TableOfAllCensuses-QcJp_Ps-.js
@@ -0,0 +1,1 @@
+import{an as r,r as t,o}from"./index-ClDiAxD1.js";import{I as a,T as c}from"./TableID-B6gLajo_.js";import{g as l}from"./CensusColumns-CL3npHlt.js";const C=({organization:s})=>{const{censuses:e}=r(),n=t.useMemo(()=>l(),[]),u=t.useMemo(()=>s?s.censuses??[]:Object.values(e),[s,e]);return o.jsx(a,{tableID:c.Censuses,entities:u,columns:n})};export{C as T};
--- assets/useFilterPanel-BCp0sACJ.js
+++
@@ -1,1 +0,0 @@
-import{r as t,ab as r}from"./index-BgPfhIUx.js";const o=()=>{const e=t.useContext(r);if(!e)throw new Error("useFilterPanel must be used within FilterPanelProvider");return e};export{o as u};
