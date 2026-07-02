55d3b0f71debe31cbcd3a621d0db7b4ad4305ba5
conradarcturus <conradarcturus@users.noreply.github.com>
2026-07-02 04:06:34 +0000

    deploy: c14772abee01bcb565d1c2422e428168fb390aa8

--- assets/ObjectDepthDisplay-DT9s4oln.js
+++
@@ -1,1 +0,0 @@
-import{O as n,o as s,U as r,bP as a,ab as h,bQ as i}from"./index-ClDiAxD1.js";const p=({object:e})=>{if(e.type===n.Census||e.type===n.Variant)return s.jsx(r,{children:"n/a"});const t=a(e);return t==null?s.jsx(r,{children:"Unknown"}):s.jsx(h,{hoverContent:s.jsx(i,{object:e,showChildren:!1}),children:t||"Root"})};export{p as O};
---
+++ assets/TableOfAllCensuses-BnwZNOK3.js
@@ -0,0 +1,1 @@
+import{an as r,r as t,o}from"./index-CS_48b3q.js";import{I as a,T as c}from"./TableID-DqxBjgT6.js";import{g as l}from"./CensusColumns-lJO72P0Z.js";const C=({organization:s})=>{const{censuses:e}=r(),n=t.useMemo(()=>l(),[]),u=t.useMemo(()=>s?s.censuses??[]:Object.values(e),[s,e]);return o.jsx(a,{tableID:c.Censuses,entities:u,columns:n})};export{C as T};
