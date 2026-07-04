be04f1ac2ad958d53f64c8ffb518eda662de3050
akash-belide <akash-belide@users.noreply.github.com>
2026-07-04 02:39:56 +0000

    deploy: 0a6dd5ab92e3f7ab8a477893e77bc8bf1654d9c4

--- assets/TableOfAllCensuses-D_2NmvT5.js
+++
@@ -1,1 +0,0 @@
-import{an as r,r as t,o}from"./index-nLpTZICJ.js";import{I as a,T as c}from"./TableID-BTbQ8Pgw.js";import{g as l}from"./CensusColumns-CUeb7lQW.js";const C=({organization:s})=>{const{censuses:e}=r(),n=t.useMemo(()=>l(),[]),u=t.useMemo(()=>s?s.censuses??[]:Object.values(e),[s,e]);return o.jsx(a,{tableID:c.Censuses,entities:u,columns:n})};export{C as T};
--- assets/useFilterPanel-CUj5fYwO.js
+++
@@ -1,1 +0,0 @@
-import{r as t,a9 as r}from"./index-nLpTZICJ.js";const o=()=>{const e=t.useContext(r);if(!e)throw new Error("useFilterPanel must be used within FilterPanelProvider");return e};export{o as u};
