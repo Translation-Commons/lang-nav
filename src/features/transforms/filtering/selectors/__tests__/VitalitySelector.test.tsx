b6b88d0c7c402eec7137a4b15f8e6d08bc54c54b
kevinmdd <medinakevin481@gmail.com>
2026-07-07 16:43:40 -0700

    Sync VitalitySelector test with master

--- src/features/transforms/filtering/selectors/__tests__/VitalitySelector.test.tsx
+++ src/features/transforms/filtering/selectors/__tests__/VitalitySelector.test.tsx
@@ -65,7 +65,7 @@ describe('VitalitySelector', () => {
 
       const expected = Object.values(LanguageISOStatus).filter((v) => typeof v === 'number');
 
-      // If there are more than 5 options expand first so all are visible
+      // If there are more than 4 options expand first so all are visible
       if (expected.length > 5) {
         await user.click(screen.getByText('Expand All'));
       }
