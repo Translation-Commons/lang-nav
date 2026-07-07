7d3821d487eb9cdf41d9e3d5e0386ad0c85d1eb2
kevinmdd <medinakevin481@gmail.com>
2026-07-07 11:02:19 -0700

    Updated ClearAllPins

--- src/widgets/controls/selectors/ClearAllPinsButton.tsx
+++ src/widgets/controls/selectors/ClearAllPinsButton.tsx
@@ -1,33 +1,37 @@
 import { BanIcon } from 'lucide-react';
 import React from 'react';
 
+import { SelectorDisplay } from '@features/params/ui/SelectorDisplayContext';
 import SelectorLabel from '@features/params/ui/SelectorLabel';
+import { getOptionStyle } from '@features/params/ui/SelectorOption';
 import usePageParams from '@features/params/usePageParams';
+import { PositionInGroup } from '@shared/lib/PositionInGroup';
 
 const ClearAllPinsButton: React.FC = () => {
-  const { pinned, updatePageParams } = usePageParams();
+    const { pinned, updatePageParams } = usePageParams();
 
-  const onClearClick = () => {
-    updatePageParams({ pinned: [] });
-  };
+    const onClearClick = () => {
+        updatePageParams({ pinned: [] });
+    };
 
-  return (
-    pinned.length > 0 && (
-      <div className="selector" style={{ display: 'flex', alignItems: 'center' }}>
-        <SelectorLabel
-          label="Clear All Pins"
-          description="Remove all pinned cards from the page."
-        />
-        <button
-          aria-label="Clear all pinned cards"
-          className="selected ClearAllPinsButton-button"
-          onClick={onClearClick}
-        >
-          <BanIcon className="ClearAllPinsButton-iconBan" size="1em" />
-        </button>
-      </div>
-    )
-  );
+    return (
+        pinned.length > 0 && (
+            <div className="selector" style={{ display: 'flex', alignItems: 'center' }}>
+                <SelectorLabel
+                    label="Clear All Pins"
+                    description="Remove all pinned cards from the page."
+                />
+                <button
+                    aria-label="Clear all pinned cards"
+                    className="selected"
+                    onClick={onClearClick}
+                    style={getOptionStyle(SelectorDisplay.Dropdown, true, PositionInGroup.Standalone)}
+                >
+                    <BanIcon className="ClearAllPinsButton-iconBan" size="1em" display='block' />
+                </button>
+            </div>
+        )
+    );
 };
 
 export default ClearAllPinsButton;
