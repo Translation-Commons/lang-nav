4ddb6199d2bac820d8db177f9a3642c6ba61ee28
Conrad Nied <conrad.logos@gmail.com>
2026-07-09 16:25:38 -0700

    Build fixes

--- src/features/params/getNewURL.ts
+++ src/features/params/getNewURL.ts
@@ -1,10 +1,10 @@
 import { getNewURLSearchParams } from './getNewURLSearchParams';
-import { PageParamsOptional } from './PageParamTypes';
+import type { PageParams } from './PageParamTypes';
 
 /**
  * Gets a fresh URL -- good for anchor links where you want to clear out existing parameters
  * and to enable people to use extra interactions (like open in new window, copy link URL, etc).
  */
-export function getNewURL(params: PageParamsOptional): string {
+export function getNewURL(params: Partial<PageParams>): string {
   return `?${getNewURLSearchParams(params).toString()}`;
 }
