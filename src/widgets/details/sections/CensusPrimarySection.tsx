055a1d85727268d5de777fd58dcb8c7a162221b8
Conrad Nied <conrad.logos@gmail.com>
2026-08-04 16:34:46 -0700

    Add some more censuses from Africa

---
+++ src/widgets/details/sections/CensusPrimarySection.tsx
@@ -0,0 +1,30 @@
+import HoverableObjectName from '@features/layers/hovercard/HoverableObjectName';
+
+import { CensusData } from '@entities/census/CensusTypes';
+
+import DetailsField from '../ui/DetailsField';
+import DetailsSection from '../ui/DetailsSection';
+
+function CensusPrimarySection({ census }: { census: CensusData }) {
+  const { territory, isoRegionCode, domain, proficiency, acquisitionOrder, languageUse } = census;
+  return (
+    <DetailsSection title="Primary Information">
+      <DetailsField title="Territory">
+        {territory != null ? (
+          <HoverableObjectName object={territory} />
+        ) : (
+          <span>{isoRegionCode}</span>
+        )}
+      </DetailsField>
+      <DetailsField title="Year">{census.yearCollected}</DetailsField>
+      {languageUse != null && <DetailsField title="Language Use">{languageUse}</DetailsField>}
+      {proficiency != null && <DetailsField title="Proficiency">{proficiency}</DetailsField>}
+      {acquisitionOrder != null && (
+        <DetailsField title="Acquisition Order">{acquisitionOrder}</DetailsField>
+      )}
+      {domain != null && <DetailsField title="Where language used">{domain}</DetailsField>}
+    </DetailsSection>
+  );
+}
+
+export default CensusPrimarySection;
--- src/widgets/reports/ReportLocalesPotential.tsx
+++ src/widgets/reports/ReportLocalesPotential.tsx
@@ -187,7 +187,7 @@ const PotentialLocalesTable: React.FC<{
           key: '% of Global Language Speakers',
           render: (object) =>
             object.pop.speaking.adjusted &&
-            (object.pop.speaking.adjusted * 100) / (object.language?.pop.speaking.estimate ?? 1),
+            (object.pop.speaking.adjusted * 100) / (object.language?.pop.overall ?? 1),
           field: Field.PercentOfOverallLanguageSpeakers,
         },
         {
