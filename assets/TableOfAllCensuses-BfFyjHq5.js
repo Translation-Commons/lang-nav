4a393c7d4f5c5931cb22f7e46eb46f15d6520d49
kevinmdd <kevinmdd@users.noreply.github.com>
2026-08-05 15:07:14 +0000

    deploy: 1bdb672278c78876288fd62b91911155e3556a63

--- assets/TableOfAllCensuses-BfFyjHq5.js
+++
@@ -1,1 +0,0 @@
-import{G as u,v as l,r as m}from"./index-lKu-ILqf.js";import{I as c,T as r}from"./TableID-nVanKvTy.js";import{g as a}from"./CensusColumns-Bv_J0D1N.js";const x=({organization:s})=>{const{censuses:e}=u(),t=l.useMemo(()=>a(),[]);return m.jsx(c,{tableID:r.Censuses,entities:s?(s==null?void 0:s.censuses)??[]:Object.values(e),columns:t})};export{x as T};
---
+++ assets/TableOfAllCensuses-p3l3imId.js
@@ -0,0 +1,1 @@
+import{G as u,v as l,r as m}from"./index-CM74WHp_.js";import{I as c,T as r}from"./TableID-CYsHRNPf.js";import{g as a}from"./CensusColumns-BOI2EO5a.js";const x=({organization:s})=>{const{censuses:e}=u(),t=l.useMemo(()=>a(),[]);return m.jsx(c,{tableID:r.Censuses,entities:s?(s==null?void 0:s.censuses)??[]:Object.values(e),columns:t})};export{x as T};
