2d4c4ce2b35fd526a0c89ab1b959f6472e90fd0c
kevinmdd <151257451+kevinmdd@users.noreply.github.com>
2026-08-13 23:26:24 +0000

    [automation] Auto-fix style with prettier and eslint

--- src/entities/locale/LocaleGrid.tsx
+++ src/entities/locale/LocaleGrid.tsx
@@ -1,6 +1,9 @@
-import { LocaleData } from '@entities/locale/LocaleTypes';
-import DetailsSection from '@widgets/details/ui/DetailsSection';
 import React from 'react';
+
+import DetailsSection from '@widgets/details/ui/DetailsSection';
+
+import { LocaleData } from '@entities/locale/LocaleTypes';
+
 import LocaleGridCell from './LocaleGridCell';
 
 type Props = {
@@ -8,11 +11,10 @@ type Props = {
 };
 
 const LocaleGrid: React.FC<Props> = ({ locales }) => {
-
   if (locales.length === 0) return null;
 
   return (
-    <DetailsSection title="Locales" >
+    <DetailsSection title="Locales">
       {/* 3x3 grid with max height and scrollable */}
       <div className="grid grid-cols-3 gap-3 p-2.5 max-h-64 overflow-y-auto">
         {locales.map((locale) => (
