a2ce2fcb0f1885f685a72ffd93d185be1e818330
conradarcturus <conradarcturus@users.noreply.github.com>
2026-08-21 20:52:22 +0000

    deploy: b5cac41cd143df1647601fd8f314cd08d939584b

---
+++ assets/TableOfAllCensuses-4_k-codg.js
@@ -0,0 +1,1 @@
+import{ag as u,v as l,r as m}from"./index-VaxLzPq4.js";import{I as c,T as r}from"./TableID-tpCVj4Vn.js";import{g as a}from"./PopulationSourceCategoryDisplay-D3TmuucJ.js";const x=({organization:s})=>{const{censuses:e}=u(),t=l.useMemo(()=>a(),[]);return m.jsx(c,{tableID:r.Censuses,entities:s?(s==null?void 0:s.censuses)??[]:Object.values(e),columns:t})};export{x as T};
--- assets/getObjectFromID-DPQETEnn.js
+++
@@ -1,1 +0,0 @@
-import{y as c,ag as n}from"./index-qFKAsIFV.js";function u(e){const{objectID:o}=c(),{censuses:s,getObject:a}=n(),t=e??o;if(t!=null)return s[t]??a(t)}export{u as g};
