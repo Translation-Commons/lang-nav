76a1625e3f7311d960fe4acfbe64430ec3624e1b
Conrad Nied <conrad.logos@gmail.com>
2026-08-21 18:36:02 -0700

    Final style updates

--- src/widgets/details/ui/DetailsSection.tsx
+++ src/widgets/details/ui/DetailsSection.tsx
@@ -4,8 +4,10 @@ import '../details.css';
 
 const DetailsSection: React.FC<PropsWithChildren<{ title: ReactNode }>> = ({ children, title }) => {
   return (
-    <div className="DetailsSection">
-      <h3 className="DetailsSectionTitle">{title}</h3>
+    <div className="flex flex-col mb-4 p-4 h-full box-border border border-[--color-button-secondary] rounded-xl">
+      <div role="heading" className="mb-0 uppercase text-xl tracking-tight">
+        {title}
+      </div>
       {children}
     </div>
   );
